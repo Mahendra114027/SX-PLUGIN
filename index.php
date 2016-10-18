f84883a277ff977353076da35d29f3c10c1bbdf3
Mahendra114027 <samcmvs11225@gmail.com>
2016-10-18 12:39:56 +0530

    Added functions

--- index.php
+++ index.php
@@ -1,17 +1,58 @@
-<!-- This is a WordPress Plugin used for email marketing -->
-<head>
-	<script type="text/js" src="js/my.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/my.css">
-</head>
-<body id="options">
-	<div id="SendXDetails">
-		<h1 id="name">SendX</h1>
-		<p id="about">360<sup>o</sup> Email Marketing Automation - Engage With Customers<br></p>
-	<div>	
-		<a id="WebSite" href="http://sendx.io/" target="blank_">  Visit Us  </a>
-		<a id="WebSupport" href="http://help.sendx.io/help_center" target="blank_">  Support  </a>
-	</div><br><br>
-		SendX Account ID <input type="text" id="aid"><br><br>
-		<button id="commit">Save Changes</button>	
-	</div>	
-</body>
+<?php
+/**
+* Plugin Name: SendX Email Marketing
+* Description: WordPress Plugin for Various services provided by SendX 
+* Version:     1.0
+* Author:      SendX
+* Author URI:  https://in.linkedin.com/in/mahendra-singh-2961b8127
+* License:     GPL2
+*/
+
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'SendX', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+var $account_id='55555';
+
+function theme_front_page_settings()
+ {
+	?>
+		<head>
+			<script type="text/js" src="js/my.js"></script>
+			<link rel="stylesheet" type="text/css" href="css/my.css">
+		</head>
+		<body id="options">
+			<div id="SendXDetails">
+				<h1 id="name">SendX</h1>
+				<p id="about">360<sup>o</sup> Email Marketing Automation - Engage With Customers<br></p>
+			<div>	
+				<a id="WebSite" href="http://sendx.io/" target="blank_">  Visit Us  </a>
+				<a id="WebSupport" href="http://help.sendx.io/help_center" target="blank_">  Support  </a>
+			</div><br><br>
+					SendX Account ID <input type="text" id="aid"><br><br>
+				<button id="commit">Save Changes</button>	
+			</div>	
+		</body>
+	<?php	
+  }
+
+
+
+  function add_async_forscript($url)
+{
+    if (strpos($url, '#asyncload')===false)
+        return $url;
+    else if (is_admin())
+        return str_replace('#asyncload', '', $url);
+    else
+        {
+        	include( WP_DRIP_DIRNAME . "/js/append.js" );
+        	return str_replace('#asyncload', '', $url)."' async='async"; 
+        }	
+}
+add_filter('clean_url', 'add_async_forscript', 11, 1);
+
+
+?>
--- js/append.js
+++ js/append.js
@@ -1,27 +1,18 @@
 <script type="text/javascript">
-  var _dcq = _dcq || [];
-  var _dcs = _dcs || {}; 
-  _dcs.account = '<?php echo $account_id ?>';
+  var _x1 = _x1 || [];
+  var _x2 = _x2 || {}; 
+  _x2.account = '<?php echo $account_id ?>';
   
   (function() {
-    var dc = document.createElement('script');
-    dc.type = 'text/javascript'; dc.async = true; 
-    dc.src = 'cdn/sendx.io<?php echo $account_id ?>.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(dc, s);
+    var y = document.createElement('script');
+    y.type = 'text/javascript';
+    y.async = true; 
+    y.src = 'cdn/sendx.io<?php echo $account_id ?>.js';
+    var scripts = document.getElementsByTagName('script')[0];
+    scripts.parentNode.insertBefore(y, scripts);
   })();
 </script>
 
 
 
 
-function add_async_forscript($url)
-{
-    if (strpos($url, '#asyncload')===false)
-        return $url;
-    else if (is_admin())
-        return str_replace('#asyncload', '', $url);
-    else
-        return str_replace('#asyncload', '', $url)."' async='async"; 
-}
-add_filter('clean_url', 'add_async_forscript', 11, 1);
